266b26920e114be3770f6148e3913d2fc1fa7f0b
Jackson <ejackson11@cherrycreekschools.org>
2023-11-09 11:44:14 -0700

    lol

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,1 +1,14 @@
 // JavaScript source code
+function button() {
+    var button = document.createElement("button");
+    button.innerHTML = "Dynamic Button";
+    button.id = "dynamicButton";
+
+    button.addEventListener("click", function () {
+        transition();
+    });
+    document.body.appendChild(button);
+}
+function transition() {
+    alert("Button Clicked!");
+}
