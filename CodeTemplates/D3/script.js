1eae5fb813a09bc270ee1154f1c54f68a36f527d
Jackson <ejackson11@cherrycreekschools.org>
2023-11-21 09:47:07 -0700

    lol

--- CodeTemplates/D3/script.js
+++ CodeTemplates/D3/script.js
@@ -1,22 +1,14 @@
-<script>
-  // Select the container
-    var container = d3.select("#dots-container");
+function changeContent() {
+    // Get the selected value from the dropdown
+    var selectedValue = document.getElementById("myDropdown").value;
 
-    // Define the data for the dots
-    var data = [
-    {color: "blue", cx: 50, cy: 50, radius: 10 },
-    {color: "red", cx: 100, cy: 50, radius: 10 },
-    {color: "green", cx: 150, cy: 50, radius: 10 },
-    ];
+    // Define content for each page
+    var pageContent = {
+        'page1': '<p>This is content for Page 1.</p>',
+        'page2': '<p>This is content for Page 2.</p>',
+        'page3': '<p>This is content for Page 3.</p>'
+    };
 
-    // Append circles to the container based on the data
-    container
-    .selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) { return d.cx; })
-    .attr("cy", function(d) { return d.cy; })
-    .attr("r", function(d) { return d.radius; })
-    .attr("fill", function(d) { return d.color; });
-</script>
+    // Update the content based on the selected page
+    document.getElementById("content").innerHTML = pageContent[selectedValue];
+}
