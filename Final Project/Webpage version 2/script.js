d27d9e627dd0b640914ab4274ee7367ed9414285
Threelivestoken <125107331+Threelivestoken@users.noreply.github.com>
2023-12-17 21:52:19 -0700

    lol

--- Final Project/Webpage version 2/script.js
+++ Final Project/Webpage version 2/script.js
@@ -1,137 +1,61 @@
-const barChartData = [
-    { label: 'Category A', value: 25 },
-    { label: 'Category B', value: 40 },
-    { label: 'Category C', value: 15 }
-    // ... add more data points as needed
-  ];
+function performSearch() {
+    var searchInputValue = document.getElementById('searchInput').value;
   
-  const scatterplotData = [
-    { x: 10, y: 20 },
-    { x: 30, y: 40 },
-    { x: 50, y: 10 }
-    // ... add more data points as needed
-  ];
-  
-  const createBarChart = () => {
-    const svgBarChart = d3.select("#bar-chart");
-  
-    const xScaleBarChart = d3.scaleBand()
-                             .domain(barChartData.map(d => d.label))
-                             .range([0, 500])
-                             .padding(0.1);
-  
-    const yScaleBarChart = d3.scaleLinear()
-                             .domain([0, d3.max(barChartData, d => d.value)])
-                             .range([300, 0]);
-  
-    // Add X axis
-    svgBarChart.append("g")
-      .attr("transform", `translate(0, ${300})`)
-      .call(d3.axisBottom(xScaleBarChart))
-      .append("text")
-      .attr("x", 500 / 2)
-      .attr("y", 40)
-      .attr("text-anchor", "middle")
-      .text("Category");
-  
-    // Add Y axis
-    svgBarChart.append("g")
-      .call(d3.axisLeft(yScaleBarChart))
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - 50)
-      .attr("x", 0 - (300 / 2))
-      .attr("text-anchor", "middle")
-      .text("Value");
-  
-    // Add bars
-    svgBarChart.selectAll("rect")
-      .data(barChartData)
-      .enter().append("rect")
-      .attr("x", d => xScaleBarChart(d.label))
-      .attr("y", d => yScaleBarChart(d.value))
-      .attr("width", xScaleBarChart.bandwidth())
-      .attr("height", d => 300 - yScaleBarChart(d.value))
-      .attr("fill", "steelblue")
-      .on("mouseover", function(event, d) {
-        d3.select(this).attr("fill", "orange");
-        showTooltip(event, `Label: ${d.label}, Value: ${d.value}`);
-      })
-      .on("mouseout", function() {
-        d3.select(this).attr("fill", "steelblue");
-        hideTooltip();
-      });
-  };
-  
-  const createScatterplot = () => {
-    const svgScatterplot = d3.select("#scatterplot");
-  
-    const xScaleScatterplot = d3.scaleLinear()
-                               .domain([0, d3.max(scatterplotData, d => d.x)])
-                               .range([0, 500]);
-  
-    const yScaleScatterplot = d3.scaleLinear()
-                               .domain([0, d3.max(scatterplotData, d => d.y)])
-                               .range([300, 0]);
-  
-    // Add X axis
-    svgScatterplot.append("g")
-      .attr("transform", `translate(0, ${300})`)
-      .call(d3.axisBottom(xScaleScatterplot))
-      .append("text")
-      .attr("x", 500 / 2)
-      .attr("y", 40)
-      .attr("text-anchor", "middle")
-      .text("X");
-  
-    // Add Y axis
-    svgScatterplot.append("g")
-      .call(d3.axisLeft(yScaleScatterplot))
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - 50)
-      .attr("x", 0 - (300 / 2))
-      .attr("text-anchor", "middle")
-      .text("Y");
-  
-    // Add circles
-    svgScatterplot.selectAll("circle")
-      .data(scatterplotData)
-      .enter().append("circle")
-      .attr("cx", d => xScaleScatterplot(d.x))
-      .attr("cy", d => yScaleScatterplot(d.y))
-      .attr("r", 6)
-      .attr("fill", "red")
-      .on("mouseover", function(event, d) {
-        d3.select(this).attr("fill", "orange");
-        showTooltip(event, `X: ${d.x}, Y: ${d.y}`);
-      })
-      .on("mouseout", function() {
-        d3.select(this).attr("fill", "red");
-        hideTooltip();
-      });
-  };
-  
-  // Tooltip functions
-  const tooltip = d3.select("body").append("div")
-                    .attr("class", "tooltip")
-                    .style("opacity", 0);
-  
-  function showTooltip(event, text) {
-    tooltip.transition()
-           .duration(200)
-           .style("opacity", 1);
-    tooltip.html(text)
-           .style("left", event.pageX + 10 + "px")
-           .style("top", event.pageY - 15 + "px");
+    // Here you can add your search functionality or redirect logic
+    // For simplicity, let's show content1
+    var content1 = document.getElementById('content1');
+    content1.style.opacity = '1'; // Ensure opacity is reset
+    content1.style.transition = 'opacity 0.5s ease-in-out'; // Add a fade transition
+    content1.style.display = 'block';
   }
   
-  function hideTooltip() {
-    tooltip.transition()
-           .duration(200)
-           .style("opacity", 0);
+  function showContent() {
+    // Hide content1 and the search bar with a fade transition
+    var content1 = document.getElementById('content1');
+    var centercontainer = document.getElementById('center-container');
+    content1.style.opacity = '0';
+    centercontainer.style.opacity='0';
+    setTimeout(function () {
+        content1.style.display = 'none';
+        centercontainer.style.display = 'none';
+    }, 500); // Wait for the transition to complete (500ms)
+  
+    // Show content2 with a delay and a fade transition
+    setTimeout(function () {
+      var content2 = document.getElementById('content2');
+      content2.style.opacity = '0';
+      content2.style.transition = 'opacity 0.5s ease-in-out'; // Add a fade transition
+      content2.style.display = 'block';
+  
+      // Reset content2 opacity after a short delay (to allow for display change)
+      setTimeout(function () {
+        content2.style.opacity = '1';
+      }, 10);
+    }, 1000); // Wait for 1 second before showing content2
   }
   
-  createBarChart();
-  createScatterplot();
+
+
+  /* for scatterplot */
+  var data = {
+    datasets: [{
+      data: [{ x: 10, y: 20 }, { x: 30, y: 40 }, { x: 50, y: 60 }, { x: 70, y: 80 }],
+      backgroundColor: 'rgba(75, 192, 192, 0.7)',
+    }]
+  };
+  
+  var options = {
+    scales: { x: { type: 'linear', position: 'bottom' }, y: { type: 'linear', position: 'left' } },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => `(${context.parsed.x}, ${context.parsed.y})`
+        }
+      }
+    }
+  };
+  
+  var ctx = document.getElementById('scatterplot').getContext('2d');
+  
+  new Chart(ctx, { type: 'scatter', data: data, options: options });
   
